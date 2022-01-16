dffbf2a6c0048fa93d4658ad0d1e0d072534c180
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2022-01-16 11:11:54 -0300

    Subindo página 'Atenas & Ártemis'

---
+++ script.js
@@ -0,0 +1,16 @@
+var a = window.document.getElementById('atenas')
+var b = window.document.getElementById('artemis')
+
+        function entrar(){
+            a.innerText = 'Essa é Atena! A deusa grega da Sabedoria.'
+            //a.style.transition = '0.5s'
+        }
+
+        function entrar2(){
+            b.innerText = 'Essa é Ártemis! A deusa grega da caça e vida selvagem.'
+        }
+
+        function sair(){
+            a.innerText = ''
+            b.innerText = ''
+        }
